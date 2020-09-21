2d7f10a17f3734f91be566184c91d27eb6e995f9
Zaiko Ann <box4ana@gmail.com>
2020-09-21 08:17:47 +0500

    Updates

---
+++ precache-manifest.f7e725a14b064afc81d16170363c00d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "57977ca15f1588d1e564",
+    "url": "/AnnaZ12/GotApp/static/css/main.bf836dbb.chunk.css"
+  },
+  {
+    "revision": "57977ca15f1588d1e564",
+    "url": "/AnnaZ12/GotApp/static/js/main.57977ca1.chunk.js"
+  },
+  {
+    "revision": "06a4fcfbcfebd3260518",
+    "url": "/AnnaZ12/GotApp/static/css/1.129f5a2f.chunk.css"
+  },
+  {
+    "revision": "06a4fcfbcfebd3260518",
+    "url": "/AnnaZ12/GotApp/static/js/1.06a4fcfb.chunk.js"
+  },
+  {
+    "revision": "e9b642440333a753c715",
+    "url": "/AnnaZ12/GotApp/static/js/runtime~main.e9b64244.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/AnnaZ12/GotApp/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/AnnaZ12/GotApp/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "eb6af177cc1077fb2fe47843e45fce40",
+    "url": "/AnnaZ12/GotApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b77df5825c360b6b75c52bb64f73c5f0.js"
+  "/AnnaZ12/GotApp/precache-manifest.f7e725a14b064afc81d16170363c00d9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/AnnaZ12/GotApp/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
